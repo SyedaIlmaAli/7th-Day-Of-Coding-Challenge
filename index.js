aab2b31517d09cf54add0b31ca2deddcced10673
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:22:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Day 07
+// Question 19
+let guestArr = ["Ilma", "Mehak", "Asma", "Hamna"];
+console.log(`I am inviting ${guestArr.length} people for Dinner`);
+//Question 20
+let places = ["Japan", "South Korea", "Russia", "Italy"];
+console.log(`I had like to visit ${places}`);
+//Question 21
+const student = {
+    name: "Ilma",
+    rollNo: 21987,
+    class: 10,
+};
+console.log(`${student.name} is studying in our school. She is currently in ${student.class} class.`);
---
+++ index.ts
@@ -0,0 +1,21 @@
+// Day 07
+
+// Question 19
+
+let guestArr : string[] = ["Ilma", "Mehak", "Asma", "Hamna"];
+console.log(`I am inviting ${guestArr.length} people for Dinner`);
+
+//Question 20
+
+let places : string[] = ["Japan", "South Korea", "Russia", "Italy"];
+console.log(`I had like to visit ${places}`);
+
+//Question 21
+
+const student = {
+    name : "Ilma",
+    rollNo : 21987,
+    class : 10,
+}
+
+console.log(`${student.name} is studying in our school. She is currently in ${student.class} class.`);
